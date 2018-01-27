b8341e8c1628c4573148f9a204dfe3846af120e0
Russell Bishop <hey@russellbishop.co.uk>
2018-01-27 17:44:38 +0000

    Adding hella things! Let's do this!

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" class="u-bgcolor-basement">
+<head>
+    <meta charset="UTF-8">
+    <title>Bandit Time, Motherfucker</title>
+    <link rel="stylesheet" type="text/css" href="/dist/css/main.css">
+</head>
+<body>
+
+    <div class="o-window u-color-blanket u-font-europa" style="margin-top: 3vw;">
+
+        <nav>
+            <div class="o-container u-flex u-jc-between u-pv-1bl">
+                <a href="/">Bandit</a>
+                <a href="/user">Archaninum</a>
+            </div>
+        </nav>
+
+        <main class="u-grow-1 u-bgcolor-floor u-borrad-6 u-borrad-6">
+
+            <ol id="disputes" class="o-container">
+
+                <h1 class="u-h2 u-color-white">Disputes</h1>
+
+                <dl>
+                    <li class="u-flex u-ai-center u-bgcolor-fold">
+                        <div class="c-player-photo">
+                        </div>
+                        <div class="u-grow-1">
+                            <dt>Rebekah Reeves</dt>
+                            <dd>4 hours to respond</dd>
+                        </div>
+                    </li>
+                </dl>
+
+            </ol>
+
+        </main>
+
+    </div>
+
+</body>
+</html>
