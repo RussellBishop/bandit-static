30a34bcbeb0aa0513c5358517e35432d92ebc8b7
Russell Bishop <hey@russellbishop.co.uk>
2018-01-31 10:02:23 +0000

    Better footer for switching

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,8 @@
+<hr style="background: #000207; height: 2px; border: none; border-bottom: 1px solid #ADC8FA50;" />
+
 <footer class="o-container u-flex u-jc-between u-ph-1bl">
 
-    <select>
+    <select class="u-color-playdough">
         <option selected="">Britannia Squash</option>
         <optgroup label="Switch club:">
             <option>Shoreditch Park Squash</option>
